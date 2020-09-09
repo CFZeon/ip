beb43264b5200269438e266d265b2a14b95bfb80
CFZeon <e0406826@u.nus.edu>
2020-09-09 15:38:52 +0800

    Add error handling and DukeException class

---
+++ src/main/java/DukeException.java
@@ -0,0 +1,28 @@
+public class DukeException extends Exception{
+
+    protected String error;
+
+    public DukeException(String error){
+        this.error=error;
+    }
+
+    public void getError(String input){
+        switch (error) {
+        case "format":
+            System.out.println("Please enter a proper action after the command! \"" + input + " [action]\"");
+            break;
+        case "deadline":
+            System.out.println("Please enter a proper deadline! \"deadline [task] /[limit] [time]\"");
+            break;
+        case "event":
+            System.out.println("Please enter a proper event! \"event [task] /[limit] [time]\"");
+            break;
+        case "todo":
+            System.out.println("Please enter a proper todo! \"todo [task]\"");
+            break;
+        default:
+            System.out.println("Please enter a valid command! Type \"help\" to see a list of commands!");
+            break;
+        }
+    }
+}
